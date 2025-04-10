b540dd66934e4ae61515ff935d8d762c2d5d7c1a
Raphael Agbo <raphaelagbo@Raphaels-MacBook-Air.local>
2025-04-10 13:05:36 -0400

    chore: trigger new deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,8 @@ const nextConfig = {
     domains: ['synvra.com', 'www.synvra.com'],
   },
   env: {
-    NEXT_PUBLIC_DOMAIN: 'www.synvra.com'
+    NEXT_PUBLIC_DOMAIN: 'www.synvra.com',
+    NEXT_PUBLIC_VERSION: '1.0.1' // Added version to trigger deploy
   },
 }
 
