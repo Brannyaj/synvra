155dbdea17c8728752a76362f4f73c7168e5f5f1
Raphael Agbo <raphaelagbo@Raphaels-MacBook-Air.local>
2025-04-11 10:50:02 -0400

    revert: restore to exact state before i18n

--- frontend/app/[lng]/layout.tsx
+++ frontend/app/[lng]/layout.tsx
@@ -1,5 +1,4 @@
 import { languages } from '../i18n/settings'
-import LanguageSwitcher from '../components/LanguageSwitcher'
 
 export function generateStaticParams() {
   return languages.map((lng) => ({ lng }))
@@ -21,14 +20,7 @@ export default function RootLayout({
         <title>Synvra</title>
       </head>
       <body>
-        <div className="min-h-screen flex flex-col">
-          {children}
-          <footer className="mt-auto py-4 bg-gray-100">
-            <div className="container mx-auto px-4">
-              <LanguageSwitcher currentLang={lng} />
-            </div>
-          </footer>
-        </div>
+        {children}
       </body>
     </html>
   )
