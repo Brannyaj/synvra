42115486ea1c3ec993c0d6ad3a744f7357a76c92
Raphael Agbo <raphaelagbo@Raphaels-MacBook-Air.local>
2025-04-11 10:34:13 -0400

    fix: simplify config and use netlify nextjs plugin

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,12 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: '',
   typescript: {
-    ignoreBuildErrors: true // Temporarily ignore TS errors for deployment
-  },
-  experimental: {
-    optimizeCss: true
+    ignoreBuildErrors: true
   }
 }
 
