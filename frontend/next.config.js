9ceb3f26111120747a92bd88a97efa7cc3a9f200
Raphael Agbo <raphaelagbo@Raphaels-MacBook-Air.local>
2025-04-11 10:38:03 -0400

    revert: remove translation changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,16 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   typescript: {
     ignoreBuildErrors: true
   }
