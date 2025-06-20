03f08b35a798b4dc02c0f754bc73e2ae64458ea9
Raphael Agbo <agbokcee1@gmail.com>
2025-06-20 06:13:24 -0400

    fix: add content to terms page to fix build

--- frontend/src/app/terms-of-service/page.tsx
+++ frontend/src/app/terms-of-service/page.tsx
@@ -1,1 +1,10 @@
- 
+export const dynamic = 'force-dynamic';
+
+export default function TermsOfServicePage() {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <p>This page is currently under construction.</p>
+    </div>
+  );
+} 
